ff83ef61df446d0de03f23f299270533bbf9cf2e
Dawid Hen <dawiddaweo@gmail.com>
2017-10-25 20:38:02 +0200

    Check heroku node env

--- config/config.js
+++ config/config.js
@@ -1,7 +1,10 @@
 // Detect environment
 console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV === 'production');
 if (process.env.NODE_ENV === 'production') {
+  console.log('prod');
   module.exports = require('./prod');
 } else {
+  console.log('dev');
   module.exports = require('./dev');
 }
